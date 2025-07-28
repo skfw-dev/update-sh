55dd9a23b9744af037fe5a05d4b463847e98484d
Ahmad Asy Syafiq <12507002@yihquan.co.id>
2025-07-28 14:53:18 +0700

    chore: rename Winget into WinGet

--- internal/pkgmgr/winget_windows.go
+++ internal/pkgmgr/winget_windows.go
@@ -9,27 +9,27 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// WinGetManager implements PackageManagerImpl for Winget on Windows.
+// WinGetManager implements PackageManagerImpl for WinGet on Windows.
 type WinGetManager struct{}
 
-// Update performs package updates using Winget.
+// Update performs package updates using WinGet.
 func (w *WinGetManager) Update(dryRun bool) error {
-	log.Info().Msg("--- Winget Package Management (Windows) ---")
+	log.Info().Msg("--- WinGet Package Management (Windows) ---")
 	if !runner.CommandExists("winget") {
-		log.Debug().Msg("Winget not found. Skipping Winget package management.")
+		log.Debug().Msg("WinGet not found. Skipping WinGet package management.")
 		return nil
 	}
 
-	// Winget upgrade flags:
+	// WinGet upgrade flags:
 	// --all: Upgrades all installed packages
-	// --include-unknown: Includes packages that are not recognized by Winget
+	// --include-unknown: Includes packages that are not recognized by WinGet
 	// --silent: Runs in silent mode
 	// --accept-package-agreements: Accepts package agreements
 	// --accept-source-agreements: Accepts source agreements
 	wingetArgs := []string{"upgrade", "--all", "--include-unknown", "--silent", "--accept-package-agreements", "--accept-source-agreements"}
-	if err := runner.RunCommand("Update Winget packages", dryRun, "winget", nil, wingetArgs...); err != nil {
+	if err := runner.RunCommand("Update WinGet packages", dryRun, "winget", nil, wingetArgs...); err != nil {
 		return err
 	}
-	log.Info().Msg("Winget maintenance complete.")
+	log.Info().Msg("WinGet maintenance complete.")
 	return nil
 }
